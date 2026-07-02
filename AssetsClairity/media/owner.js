b3ac2ee5c35ee11d4682aa2f8aee67ba2813e7e7
dark-end5 <christtheanchormentors@gmail.com>
2026-07-03 02:05:30 +0300

    Add global owner and bot name variables

---
+++ AssetsClairity/media/owner.js
@@ -0,0 +1,5 @@
+// do not change this, friend — because
+// there is already a request when scanning pairing
+global.ownername = "LESTA";
+global.owner = "254706519089";
+global.botname = "DARK-END10 V1.3.0";
